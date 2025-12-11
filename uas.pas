63a35e1f7a5338bbfcb2d819a2355c0da9b8e893
dwaiapranantaginting <dwaiapranantaginting07@gmail.com>
2025-12-11 20:10:22 +0700

    added a lil design

--- uas.pas
+++ uas.pas
@@ -5,6 +5,7 @@ type
         nama: string;
         p_kebutuhan: longint;
         saldo: longint;
+        persen : real;
         end;
 const
     max = 100;
@@ -20,6 +21,7 @@ procedure inputanUser;
 begin
 clrscr;
     write('Masukkan Pemasukkan Bulanan kamu : '); readln(pemasukan);
+    writeln;
     write('Masukkan Banyak jenis kebutuhan kamu (misalnya makan, skincare, tabungan <== terhitung 3) : '); readln(b_kebutuhan);
     for i := 1 to b_kebutuhan do begin
         write('jenis kebutuhan ke-', i, ' :'); readln(butuh[i].nama);
@@ -35,10 +37,33 @@ clrscr;
             total := total + p_kebutuhan;
         end;
     end;
-    writeln;
-    writeln('total pengeluaran kamu :', total);
+    sisa := pemasukan - total;
+
+    for i := 1 to b_kebutuhan do
+    begin
+    with butuh[i] do
+    begin
+    persen := (p_kebutuhan / total) * 100;
+        end;
+    end; 
+end;
+
+procedure tabell;
+begin
+    writeln('============ TABEL PENGELUARAN ===============');
+    writeln('NO     NAMA KEBUTUHAN     BIAYA     PERSENTASE');
+    writeln('----------------------------------------------');
+
+    for i := 1 to b_kebutuhan do
+    begin
+        with butuh[i] do
+        begin
+        writeln(i, '      ', nama, '          ', p_kebutuhan, '          ', persen:0:2, '%') ;
+        end;
+    end;
 end;
 
 begin
 inputanUser;
+tabell;
 end.
