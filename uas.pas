0e6b6204d1eab247f9e7d85eff9bdb5b0330e142
dwaiapranantaginting <dwaiapranantaginting07@gmail.com>
2025-12-11 21:19:18 +0700

    last updt before sleep

--- uas.pas
+++ uas.pas
@@ -4,8 +4,7 @@ type
     kebutuhan=record
         nama: string;
         p_kebutuhan: longint;
-        saldo: longint;
-        persen : real;
+        persen : extended;
         end;
 const
     max = 100;
@@ -13,15 +12,24 @@ var
     i: integer;
     butuh: array [1..max] of kebutuhan;
     b_kebutuhan : integer;
-    pemasukan,sisa: longint;
-    total : integer;
+    pemasukan,sisa, total: longint;
+    ulangan : char;
+    persen_total : real;
 
 procedure inputanUser;
 begin
 clrscr;
     write('Masukkan Pemasukkan Bulanan kamu : '); readln(pemasukan);
     writeln;
+    repeat
     write('Masukkan Banyak jenis kebutuhan kamu (misalnya makan, skincare, tabungan <== terhitung 3) : '); readln(b_kebutuhan);
+    if b_kebutuhan > max then 
+    writeln('Input salah!!!');
+    writeln;
+    until b_kebutuhan < max;
+    if b_kebutuhan >= 10 then
+    writeln('kebutuhan banyak banget twin :( dunia kejam, tetap semangat');
+
     for i := 1 to b_kebutuhan do begin
         write('jenis kebutuhan ke-', i, ' :'); readln(butuh[i].nama);
         end;
@@ -31,12 +39,13 @@ clrscr;
         total := 0;
     for i:= 1 to b_kebutuhan do begin
         with butuh[i] do begin
-            write (nama,': '); readln(p_kebutuhan);
+            write ('pengeluaran ', nama ,' : '); readln(p_kebutuhan);
 
             total := total + p_kebutuhan;
         end;
     end;
     sisa := pemasukan - total;
+    writeln;
 end;
 
 procedure Persentase;
@@ -50,9 +59,17 @@ begin
     end;
 end; 
 
+procedure hitungpersentotal;
+begin
+    persen_total := (total / pemasukan) * 100;
+end;
+
+
 
 procedure tabell;
 begin
+    Persentase;
+    hitungpersentotal;
     writeln('================= TABEL PENGELUARAN =================');
     writeln('No   Nama Kebutuhan        Biaya          Persentase');
     writeln('------------------------------------------------------');
@@ -66,13 +83,16 @@ begin
     end;
 
     writeln('------------------------------------------------------');
-    writeln('Total Pengeluaran : ', total);
-    writeln('Sisa Uang         : ', sisa);
+    writeln('Total Pengeluaran                        : ', total);
+    writeln('persenan pengeluaran terhadap pemasukkan : ', persen_total:0:2, '%');
+    writeln('Sisa Uang                                : ', sisa);
     writeln('======================================================');
 end;
 
 begin
+repeat
 inputanUser;
-Persentase;
 tabell;
+write('ingin mengulang program??(y/n)'); readln(ulangan)
+until (ulangan <> 'y') or (ulangan <> 'Y');
 end.
