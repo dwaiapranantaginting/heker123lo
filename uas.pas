739240aeadc82a3253201fd2c837e9d045ff0857
dwaiapranantaginting <dwaiapranantaginting07@gmail.com>
2025-12-13 18:36:50 +0700

    by rizqa

--- uas.pas
+++ uas.pas
@@ -3,108 +3,121 @@ uses crt;
 type
     kebutuhan=record
         nama: string;
-        persen: real;
-        hasil : real;
+        p_kebutuhan: longint;
+        persen : extended;
+        porsi: integer;
+        pemasukan_perkebutuhan: longint;
+        sisa_perkebutuhan: longint;
         end;
+const
+    max = 100;
+    max_persen = 100;
 var
     i: integer;
-    butuh: array [1..100] of kebutuhan;
-    pemasukan: longint;
-    banyak : integer;
-    total : real;
-    sisa  : real;
+    butuh: array [1..max] of kebutuhan;
+    b_kebutuhan, total_porsi : integer;
+    pemasukan,sisa, total: longint;
+    ulangan : char;
+    persen_total : real;
 
-procedure inputan;
+procedure inputanUser;
 begin
-    clrscr;
-
-    write ('Masukkan pemasukan bulanan : ');
-    readln (pemasukan);
-
-    write ('Masukkan banyak kebutuhan : ');
-    readln (banyak);
-
-    for i := 1 to banyak do
-    begin
-        write ('Jenis kebutuhan ke-', i, ' : ');
-        readln(butuh[i].nama);
-    end;
-
+clrscr;
+    write('Masukkan Pemasukkan Bulanan kamu : '); readln(pemasukan);
     writeln;
-    writeln ('Masukkan persentase (contoh: 50, 20, 15)');
-    total := 0;
+    repeat
+    write('Masukkan Banyak jenis kebutuhan kamu (misalnya makan, skincare, tabungan <== terhitung 3) : '); readln(b_kebutuhan);
+    if b_kebutuhan > max then 
+    writeln('Input salah!!!');
+    writeln;
+    until b_kebutuhan < max;
+    if b_kebutuhan >= 10 then
+    writeln('kebutuhan banyak banget twin :( dunia kejam, tetap semangat');
 
-    for i := 1 to banyak do
+    for i := 1 to b_kebutuhan do begin
+        write('jenis kebutuhan ke-', i, ' :'); readln(butuh[i].nama);
+        end;
+        writeln;
+
+        writeln('pilih pengeluaran setiap jenis kebutuhan (misalnya skincare : 100000)');
+        total := 0;
+    for i:= 1 to b_kebutuhan do begin
+        with butuh[i] do begin
+            write ('pengeluaran ', nama ,' : '); readln(p_kebutuhan);
+
+            total := total + p_kebutuhan;
+        end;
+    end;
+    sisa := pemasukan - total;
+    writeln;
+end;
+
+procedure Persentase;
+begin
+    for i := 1 to b_kebutuhan do
     begin
-        write (butuh[i].nama, ' : ');
-        readln (butuh[i].persen);
-        total := total + butuh[i].persen;
+    with butuh[i] do
+    begin
+    persen := (p_kebutuhan / total) * 100;
+        end;
+    end;
+end; 
+
+procedure hitungpersentotal;
+begin
+    persen_total := (total / pemasukan) * 100;
+end;
+
+procedure porsikebutuhan;
+begin
+repeat
+    total_porsi:=0;
+    writeln ('Masukkan porsi masing-masing kebutuhan(contoh: 50% dimasukkan dalam bentuk 50):');
+    for i:= 1 to b_kebutuhan do
+    with butuh[i] do
+    begin
+        write (nama,': '); readln (porsi);
+        total_porsi:= total_porsi+porsi;
+        if total_porsi > max_persen then
+        writeln ('Porsi kebutuhan melebihi kapasitas!! Silakan input ulang');
+        pemasukan_perkebutuhan:= round(pemasukan*(porsi/100));
+    end;
+until total_porsi <= max_persen;
+writeln ('===== PORSI UANG PER KEBUTUHAN DALAM SATU BULAN =====');
+for i:=1 to b_kebutuhan do
+    begin
+        writeln (butuh[i].nama,': ',butuh[i].pemasukan_perkebutuhan);
+        butuh[i].sisa_perkebutuhan:= butuh[i].pemasukan_perkebutuhan - butuh[i].p_kebutuhan;
     end;
 end;
-
-
-procedure hitpersen;
+procedure tabell;
 begin
-    inputan;
-
-    for i := 1 to banyak do
-    butuh[i].hasil := (butuh[i].persen / 100) * pemasukan;
-end;
-
-procedure cekpersen;
-begin
-    if total < 100 then
-    begin
-        sisa := ((100 - total) / 100) * pemasukan;
-    end
-    else if total > 100 then
-    begin
-        sisa := -((total - 100) / 100) * pemasukan;
-    end
-    else
-    begin
-        sisa := 0;
-    end;
-end;
-
-
-procedure tabel;
-begin
-    clrscr;
-
+    Persentase;
+    hitungpersentotal;
     writeln('================= TABEL PENGELUARAN =================');
-    writeln('No   Nama Kebutuhan        Biaya          Persentase');
-    writeln('----------------------------------------------------');
+    writeln('No   Nama Kebutuhan        Biaya          Persentase          Sisa Saldo');
+    writeln('------------------------------------------------------');
 
-    for i := 1 to banyak do
+    for i := 1 to b_kebutuhan do
     begin
-        writeln(
-            i:2, '   ',
-            butuh[i].nama:15, '     Rp ',
-            butuh[i].hasil:10:0, '     ',
-            butuh[i].persen:6:2, '%'
-        );
+        with butuh[i] do
+        begin
+        writeln(i, '      ', nama:15, '          ', p_kebutuhan:10, '          ', persen:6:2, '%', '          ', sisa_perkebutuhan:10) ;
+        end;
     end;
 
-    writeln('----------------------------------------------------');
-    writeln('Total Persentase : ', total:0:2, '%');
-
-    cekpersen;
-
-    if sisa > 0 then
-        writeln('SISA UANG        : Rp ', sisa:0:0)
-    else if sisa < 0 then
-        writeln('KEKURANGAN UANG  : Rp ', abs(sisa):0:0)
-    else
-        writeln('Uang PAS, tidak ada sisa');
-
-    writeln('====================================================');
-    readln;
+    writeln('------------------------------------------------------');
+    writeln('Total Pengeluaran                        : ', total);
+    writeln('persenan pengeluaran terhadap pemasukkan : ', persen_total:0:2, '%');
+    writeln('Sisa Uang                                : ', sisa);
+    writeln('======================================================');
 end;
 
-
-
 begin
-    hitpersen;
-    tabel;
+repeat
+inputanUser;
+porsikebutuhan;
+tabell;
+write('ingin mengulang program??(y/n)'); readln(ulangan)
+until (ulangan <> 'y') or (ulangan <> 'Y');
 end.
