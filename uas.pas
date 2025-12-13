4a516ad43db3b0d6590cc76ed1eb19c083cad818
rizqafaiza <rizqafaizaraihanah@gmail.com>
2025-12-13 18:30:07 +0700

    tambahan dikit

--- uas.pas
+++ uas.pas
@@ -5,13 +5,17 @@ type
         nama: string;
         p_kebutuhan: longint;
         persen : extended;
+        porsi: integer;
+        pemasukan_perkebutuhan: longint;
+        sisa_perkebutuhan: longint;
         end;
 const
     max = 100;
+    max_persen = 100;
 var
     i: integer;
     butuh: array [1..max] of kebutuhan;
-    b_kebutuhan : integer;
+    b_kebutuhan, total_porsi : integer;
     pemasukan,sisa, total: longint;
     ulangan : char;
     persen_total : real;
@@ -64,21 +68,41 @@ begin
     persen_total := (total / pemasukan) * 100;
 end;
 
-
-
+procedure porsikebutuhan;
+begin
+repeat
+    total_porsi:=0;
+    writeln ('Masukkan porsi masing-masing kebutuhan(contoh: 50% dimasukkan dalam bentuk 50):');
+    for i:= 1 to b_kebutuhan do
+    with butuh[i] do
+    begin
+        write (nama,': '); readln (porsi);
+        total_porsi:= total_porsi+porsi;
+        if total_porsi > max_persen then
+        writeln ('Porsi kebutuhan melebihi kapasitas!! Silakan input ulang');
+        pemasukan_perkebutuhan:= round(pemasukan*(porsi/100));
+    end;
+until total_porsi <= max_persen;
+writeln ('===== PORSI UANG PER KEBUTUHAN DALAM SATU BULAN =====');
+for i:=1 to b_kebutuhan do
+    begin
+        writeln (butuh[i].nama,': ',butuh[i].pemasukan_perkebutuhan);
+        butuh[i].sisa_perkebutuhan:= butuh[i].pemasukan_perkebutuhan - butuh[i].p_kebutuhan;
+    end;
+end;
 procedure tabell;
 begin
     Persentase;
     hitungpersentotal;
     writeln('================= TABEL PENGELUARAN =================');
-    writeln('No   Nama Kebutuhan        Biaya          Persentase');
+    writeln('No   Nama Kebutuhan        Biaya          Persentase          Sisa Saldo');
     writeln('------------------------------------------------------');
 
     for i := 1 to b_kebutuhan do
     begin
         with butuh[i] do
         begin
-        writeln(i, '      ', nama:15, '          ', p_kebutuhan:10, '          ', persen:6:2, '%') ;
+        writeln(i, '      ', nama:15, '          ', p_kebutuhan:10, '          ', persen:6:2, '%', '          ', sisa_perkebutuhan:10) ;
         end;
     end;
 
@@ -92,7 +116,9 @@ end;
 begin
 repeat
 inputanUser;
+porsikebutuhan;
 tabell;
 write('ingin mengulang program??(y/n)'); readln(ulangan)
 until (ulangan <> 'y') or (ulangan <> 'Y');
 end.
+
